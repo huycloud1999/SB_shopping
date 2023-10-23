3bb5a888d475e20aa9c3401c061be0b0c7133ce9
SieuBuong <huycloud1999@gmail.com>
2023-10-24 01:00:30 +0700

    frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+
 import "./globals.css";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemProvider from "@/providers/ThemProvider";
+
 export const metadata: Metadata = {
-  title: "Huy Blog",
-  description: "Blog Nextjs13!",
+  title: "SieuBuong",
+  description: "Shopping",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
